c2f9535b3981811a1aa03d10924a69c40ca5003e
Jeni Tennison <jeni@theodi.org>
2014-11-08 12:31:44 +0000

    added lists of maps

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -3,17 +3,40 @@ var searchBaseUrl = "http://hansard.services.digiminster.com/members/contributio
 $('#hansardSearchForm').submit(
 	function( event ) {
 		var searchText = $('#hansardSearchText').val();
+		var $searchResults = $('#hansardSearchResults');
+		var $mpResults = $('#hansardMPs');
 		$.getJSON( searchBaseUrl + searchText, function( data ) {
 			var items = [];
+			var mps = {};
+			var mpItems = [];
+			var mpList = [];
 			$.each( data.Results, function( key, val ) {
-			    items.push( "<li class='list-group-item'>" + val.ContributionText + "</li>" );
-			  });
-			 
-			  $('#hansardSearchResults').html($( "<ul />", {
-			  	class: 'list-group',
-			    html: items.join( "" )
-			  }));
-  		});
+				items.push( "<li class='list-group-item'>" + val.ContributionText + "</li>" );
+				if (mps[val.MemberId] === undefined) {
+					mps[val.MemberId] = { mp: val.MemberId, count: 1 };
+				} else {
+					mps[val.MemberId]['count']++;
+				}
+			});
+
+			$searchResults.html($( "<ul />", {
+				class: 'list-group',
+			  html: items.join( "" )
+			}));
+			
+			$.each(mps, function(key, val) {
+				mpItems.push(val);
+			});
+
+			$.each(mpItems.sort(function(a,b) { return b.count - a.count }), function(key, val) {
+				mpList.push("<li class='list-group-item'><span class='badge'>" + val.count + "</span>" + val.mp + "</li>");
+			});
+
+			$mpResults.html($( "<ul />", {
+				class: 'list-group',
+			  html: mpList.join( "" )
+			}));
+  	});
 		event.preventDefault();
 	}
 );
