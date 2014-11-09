d53722db62274207d1e518cb2957cf4be07d8436
Jeni Tennison <jeni@theodi.org>
2014-11-09 10:50:00 +0000

    use one big mp json list

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -23,88 +23,90 @@ $('#hansardSearchForm').submit(
 		$searchResults.html("<h4>Debates about " + searchText + "</h4>");
 		$mpResults.html("<h4>" + mpsOrLords + " talking about " + searchText + "</h4>");
 
-		$.getJSON(searchUrl, function( data ) {
-			var debates = {};
-			var debateItems = [];
-			var mps = {};
-			var mpItems = [];
-			var $searchResultList = $("<div class='panel-group' id='hansardAccordian' role='tablist' aria-multiselectable='true' />");
-			var $mpResultTable = $("<table class='table' />");
-			var $mpResultList = $("<div class='panel panel-default' />").append($mpResultTable);
+		$.getJSON('data/members/all.json', function ( mpDatas ) {
 
-			$.each( data.Results.sort(function(a,b) { return a.ContributionId - b.ContributionId }), function( key, val ) {
-				if (debates[val.DebateSectionId] === undefined) {
-					debates[val.DebateSectionId] = [val];
-				} else {
-					debates[val.DebateSectionId].push(val);
-				}
-				if (mps[val.MemberId] === undefined) {
-					mps[val.MemberId] = { mp: val.MemberId, count: 1 };
-				} else {
-					mps[val.MemberId]['count']++;
-				}
-			});
-			
-			$.each(debates, function(key, val) {
-				debateItems.push(val);
-			});
+			$.getJSON(searchUrl, function( data ) {
+				var debates = {};
+				var debateItems = [];
+				var mps = {};
+				var mpItems = [];
+				var $searchResultList = $("<div class='panel-group' id='hansardAccordian' role='tablist' aria-multiselectable='true' />");
+				var $mpResultTable = $("<table class='table' />");
+				var $mpResultList = $("<div class='panel panel-default' />").append($mpResultTable);
 
-			$.each(debateItems.sort(function(a,b) { return b.length - a.length }), function(key, val) {
-				var $panel = $("<div class='panel panel-default' />");
-				var $panelCollapse = $("<div id='debate-" + val[0].DebateSectionId + "-speeches' class='panel-collapse collapse' role='tabpanel' aria-labelledby='debate-" + val[0].DebateSectionId + "' />");
-				var $table = $("<table class='table' />");
-				var date = new Date(val[0].SittingDate);
+				$.each( data.Results.sort(function(a,b) { return a.ContributionId - b.ContributionId }), function( key, val ) {
+					if (debates[val.DebateSectionId] === undefined) {
+						debates[val.DebateSectionId] = [val];
+					} else {
+						debates[val.DebateSectionId].push(val);
+					}
+					if (mps[val.MemberId] === undefined) {
+						mps[val.MemberId] = { mp: val.MemberId, count: 1 };
+					} else {
+						mps[val.MemberId]['count']++;
+					}
+				});
+				
+				$.each(debates, function(key, val) {
+					debateItems.push(val);
+				});
 
-				$panel.append("<div class='panel-heading' role='tab', id='debate-" + val[0].DebateSectionId + "'><a data-toggle='collapse' data-parent='#hansardAccordian' href='#debate-" + val[0].DebateSectionId + "-speeches' aria-expanded='true' aria-controls='debate-" + val[0].DebateSectionId + "-speeches'><div>" + date.toLocaleDateString() + " " + val[0].DebateSection + "<span class='badge pull-right'>" + val.length + "</span></div></a></div>");
+				$.each(debateItems.sort(function(a,b) { return b.length - a.length }), function(key, val) {
+					var $panel = $("<div class='panel panel-default' />");
+					var $panelCollapse = $("<div id='debate-" + val[0].DebateSectionId + "-speeches' class='panel-collapse collapse' role='tabpanel' aria-labelledby='debate-" + val[0].DebateSectionId + "' />");
+					var $table = $("<table class='table' />");
+					var date = new Date(val[0].SittingDate);
 
-				$.each(val, function(key, val) {
+					$panel.append("<div class='panel-heading' role='tab', id='debate-" + val[0].DebateSectionId + "'><a data-toggle='collapse' data-parent='#hansardAccordian' href='#debate-" + val[0].DebateSectionId + "-speeches' aria-expanded='true' aria-controls='debate-" + val[0].DebateSectionId + "-speeches'><div>" + date.toLocaleDateString() + " " + val[0].DebateSection + "<span class='badge pull-right'>" + val.length + "</span></div></a></div>");
+
+					$.each(val, function(key, val) {
+						var $row = $("<tr />");
+						var $mpCell = $("<td class='hansardMP' />");
+						var $textCell = $("<td class='hansardText' />");
+
+						var mpData = mpDatas[val.MemberId];
+						$mpCell.html(memberLink(mpData) + "<br>" + memberParty(mpData));
+
+						$.getJSON(contributionBaseUrl + val.ContributionId + ".json", function ( contributionData ) {
+							$textCell.html("<p class='contributionText'>" + contributionData[0].ContributionText + "</p><p><a target='_blank' href='http://membersdataportal.digiminster.com/Debates/Commons/" + contributionData[0].SittingDate.substr(0,10) + "/" + contributionData[0].DebateSectionId + "#contribution-" + contributionData[0].ContributionId + "'>Read more <span class='glyphicon glyphicon-new-window'></span></a></p>");
+						});
+
+						$row.append($mpCell);
+						$row.append($textCell);
+						$table.append($row);
+					});
+
+					$panelCollapse.append($table);
+					$panel.append($panelCollapse);
+					$searchResultList.append($panel);
+				});
+
+				$searchResults.append($searchResultList);
+				
+				$.each(mps, function(key, val) {
+					mpItems.push(val);
+				});
+
+				$.each(mpItems.sort(function(a,b) { return b.count - a.count }), function(key, val) {
 					var $row = $("<tr />");
 					var $mpCell = $("<td class='hansardMP' />");
-					var $textCell = $("<td class='hansardText' />");
+					var $partyCell = $("<td />");
+					var $countCell = $("<td><span class='badge'>" + val.count + "</span></td>");
 
-					$.getJSON(mpBaseUrl + val.MemberId + ".json", function (mpData) {
-						$mpCell.html(memberLink(mpData) + "<br>" + memberParty(mpData));
-					});
-
-					$.getJSON(contributionBaseUrl + val.ContributionId + ".json", function ( contributionData ) {
-						$textCell.html("<p class='contributionText'>" + contributionData[0].ContributionText + "</p><p><a target='_blank' href='http://membersdataportal.digiminster.com/Debates/Commons/" + contributionData[0].SittingDate.substr(0,10) + "/" + contributionData[0].DebateSectionId + "#contribution-" + contributionData[0].ContributionId + "'>Read more <span class='glyphicon glyphicon-new-window'></span></a></p>");
-					});
-
-					$row.append($mpCell);
-					$row.append($textCell);
-					$table.append($row);
-				});
-
-				$panelCollapse.append($table);
-				$panel.append($panelCollapse);
-				$searchResultList.append($panel);
-			});
-
-			$searchResults.append($searchResultList);
-			
-			$.each(mps, function(key, val) {
-				mpItems.push(val);
-			});
-
-			$.each(mpItems.sort(function(a,b) { return b.count - a.count }), function(key, val) {
-				var $row = $("<tr />");
-				var $mpCell = $("<td class='hansardMP' />");
-				var $partyCell = $("<td />");
-				var $countCell = $("<td><span class='badge'>" + val.count + "</span></td>");
-
-				$.getJSON(mpBaseUrl + val.mp + ".json", function ( mpData ) {
+					var mpData = mpDatas[val.mp];
 					$mpCell.html(memberLink(mpData));
 					$partyCell.html(memberParty(mpData));
+
+					$row.append($mpCell);
+					$row.append($partyCell);
+					$row.append($countCell);
+					$mpResultTable.append($row);
 				});
 
-				$row.append($mpCell);
-				$row.append($partyCell);
-				$row.append($countCell);
-				$mpResultTable.append($row);
-			});
+				$mpResults.append($mpResultList);
+	  	});
 
-			$mpResults.append($mpResultList);
-  	});
+		});
 
 		event.preventDefault();
 	}
